d9b7a5c3fc8c7333c34a556dd7d0d2697d2b22ec
sporeball <sporeballdev@gmail.com>
2025-09-14 00:34:12 -0400

    tunnels

--- procgen.py
+++ procgen.py
@@ -20,14 +20,15 @@ class Room:
   @property
   def inner(self) -> Tuple[slice, slice]:
     return slice(self.x1 + 1, self.x2 - 1), slice(self.y1 + 1, self.y2 - 1)
-  @property # 3
+  @property
   def center(self) -> Tuple[int, int]:
-    pass
+    return int((self.x1 + self.x2 - 1) / 2), int((self.y1 + self.y2 - 1) / 2)
 
 def generate_world_map(map_width: int, map_height: int) -> WorldMap:
   wm = WorldMap(map_width, map_height)
-  r1 = Room(x=1, y=1, width=3, height=3)
-  r2 = Room(x=1, y=6, width=5, height=5)
+  r1 = Room(x=1, y=1, width=7, height=7)
+  r2 = Room(x=10, y=15, width=9, height=9)
   wm.place_room(r1)
   wm.place_room(r2)
+  wm.dig_tunnel(r1, r2)
   return wm
--- world_map.py
+++ world_map.py
@@ -1,11 +1,12 @@
 from __future__ import annotations
-from typing import TYPE_CHECKING
+import random
+from typing import TYPE_CHECKING, Iterator, Tuple
 
 if TYPE_CHECKING:
   from procgen import Room
 
 import numpy as np
-from tcod.console import Console
+import tcod
 
 import tile
 
@@ -13,11 +14,37 @@ class WorldMap:
   def __init__(self, width: int, height: int):
     self.width = width
     self.height = height
-    self.tiles = np.full((width, height), fill_value=tile.floor, order="F")
+    self.tiles = np.full((width, height), fill_value=tile.void, order="F")
+  def neighbors(self, x: int, y: int) -> list[Tuple[int, int]]:
+    return [(x - 1, y - 1), (x, y - 1), (x + 1, y - 1), (x - 1, y), (x + 1, y), (x - 1, y + 1), (x, y + 1), (x + 1, y + 1)]
+  def void_neighbors(self, x: int, y: int) -> Iterator[Tuple[int, int]]:
+    for nx, ny in self.neighbors(x, y):
+      if np.array_equal(self.tiles[nx, ny], tile.void):
+        yield nx, ny
   def place_room(self, room: Room) -> None:
     self.tiles[room.outer] = tile.wall
     self.tiles[room.inner] = tile.floor
+  def dig_tunnel(self, r1: Room, r2: Room) -> None:
+    # 0 < R < 0.5 :: horizontal, then vertical
+    # 0.5 <= R < 1.0 :: vertical, then horizontal
+    R = random.random()
+    # room centers
+    x1, y1 = r1.center
+    x2, y2 = r2.center
+    # find corner
+    if R < 0.5:
+      corner_x, corner_y = x2, y1
+    else:
+      corner_x, corner_y = x1, y2
+    # find lines of sight
+    l1 = tcod.los.bresenham((x1, y1), (corner_x, corner_y)).tolist()
+    l2 = tcod.los.bresenham((corner_x, corner_y), (x2, y2)).tolist()
+    # dig tunnel
+    for x, y in l1 + l2:
+      self.tiles[x, y] = tile.floor
+      for vx, vy in self.void_neighbors(x, y):
+        self.tiles[vx, vy] = tile.wall
   def in_bounds(self, x: int, y: int) -> bool:
     return 0 <= x < self.width and 0 <= y < self.height
-  def render(self, console: Console) -> None:
+  def render(self, console: tcod.console.Console) -> None:
     console.rgb[0:self.width, 0:self.height] = self.tiles["dark"]
